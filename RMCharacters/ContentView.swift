550673e3dd473aaf41081ac55a02c600d3aafb4c
Vitalii Tonchak <tonchakvi@gmail.com>
2024-04-17 12:58:38 +0200

    Initial Commit

---
+++ RMCharacters/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  RMCharacters
+//
+//  Created by Vitaliy Tonchak on 17/4/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ RMCharacters/RMCharactersApp.swift
@@ -0,0 +1,17 @@
+//
+//  RMCharactersApp.swift
+//  RMCharacters
+//
+//  Created by Vitaliy Tonchak on 17/4/24.
+//
+
+import SwiftUI
+
+@main
+struct RMCharactersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
